e2d5d3426d93f2bea91ab5cf810581e489e6ad14
max_g <maxgongob@gmail.com>
2025-04-03 16:56:42 -1000

    Completed exercise

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 const epicMix = [
-  "Time to Pretend",
-  "O-o-oh Child",
-  "Wish You Were Here",
-  "Heroes",
-  "I Put a Spell on You",
-  "Call Me",
-  "Paper Planes",
-  "Jolene",
-  "You Don't Own Me",
-  "Fast Car",
-  "Run the World (Girls)",
-  "Superstition"
+  "Build you up - 50 cent",
+  "Creepin - Jamie Foxx",
+  "Can't take my eyes off of you - Lauryn Hill",
+  "Blame it - T-pain/Jamie Foxx",
+  "Beauty - Dru Hill",
+  "Never - Green",
+  "You love me good - Ekolu",
+  "You and I - Natural Vibrations",
+  "Empress touch - Likkle Jordee",
+  "Your man - Joji",
+  "Patience - Damian Marley",
+  "Herside story - Goldlink",
+  "Beautiful - Snoop Dogg/Pharell"
 ];
 
 const mixList = document.querySelector(".mix");
@@ -20,4 +21,16 @@ const total = document.querySelector(".total");
 button.addEventListener("click", function () {
   mixList.classList.remove("hide");
   button.classList.add("hide");
+  mixInfo(epicMix);
 });
+
+total.innerText = `${epicMix.length} impactful songs.`
+
+const mixInfo = function (mix) {
+  mix.forEach(function (song, index) {
+    let li = document.createElement("li");
+    li.classList.add("song");
+    li.innerHTML = `<span class="song-number">#${index + 1}</span> ${song}`
+    mixList.append(li);
+  });
+};
